f601810abb8fa11cec91b6df86081e50a1ffaf0a
Stan Krowinski <hello@krowinski.com>
2017-05-30 15:38:20 +0200

    Initial commit

---
+++ Arch/RandomGeneration/Objects/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arch {
+  namespace RandomGeneration {
+    namespace Objects {
+      public class Rotate : MonoBehaviour {
+
+      	// Use this for initialization
+      	void Start () {
+          RotateObject();
+      	}
+      	
+      	// Update is called once per frame
+      	void Update () {
+      		
+      	}
+
+        private void RotateObject() {
+          float rotationY = 90.0f * (float)Random.Range (0, 4);
+          transform.Rotate (0, rotationY, 0);
+        }
+      }
+    }
+  }
+}
---
+++ Arch/RandomGeneration/Objects/Scale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arch {
+  namespace RandomGeneration {
+    namespace Objects {
+      public class Scale : MonoBehaviour {
+        public float minScale = 0.75f;
+        public float maxScale = 1.0f;
+
+        void Start () {
+          ScaleObject();
+        }
+
+        private void ScaleObject() {
+          float scaleFactor = Random.Range (minScale, maxScale);
+          transform.localScale = Vector3.one * scaleFactor;
+        }
+      }
+    }
+  }
+}
