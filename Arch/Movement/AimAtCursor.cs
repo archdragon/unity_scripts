9e9360e4e090657fadb5d06b250778963e2eab84
Stan Krowinski <przemyslaw.krowinski@profinda.com>
2017-06-03 19:28:38 +0200

    Add basic character movement scripts

---
+++ Arch/Movement/AimAtCursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimAtCursor : MonoBehaviour {
+	public Plane groundPlane;
+	public Transform crosshair;
+
+	// Use this for initialization
+	void Start () {
+		groundPlane = new Plane(Vector3.up, Vector3.zero);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float rayDistance;
+		if (groundPlane.Raycast (ray, out rayDistance)) {
+			crosshair.position = ray.GetPoint (rayDistance);
+			transform.LookAt (crosshair.position);
+		}
+	}
+}
