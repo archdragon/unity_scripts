f04d01ce911094e58cd0c93b6b6f2b2693964c70
Stan Krowinski <przemyslaw.krowinski@profinda.com>
2017-06-03 19:27:24 +0200

    Add Tiled editor import / level generation scripts

---
+++ Arch/LevelGeneration/LayerObjectSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arch {
+  namespace LevelGeneration {
+    public class LayerObjectSpawner : MonoBehaviour {
+
+    	// Use this for initialization
+      public static void Spawn (LayerObjectData layerObject, LevelData levelData) {
+        string prefabName = PrefabName (layerObject.gid);
+        GameObject instance = Instantiate(Resources.Load("Prefabs/LayerObjects/" + prefabName, typeof(GameObject))) as GameObject;
+        instance.transform.position = ObjectPosition(layerObject, levelData);
+    	}
+    	
+    	// Update is called once per frame
+      private static string PrefabName (int globalId) {
+        string name;
+
+        switch (globalId) {
+        default:
+          name = "Stone";
+          break;
+        }
+
+        return name;
+    	}
+
+      private static Vector3 ObjectPosition(LayerObjectData layerObject, LevelData levelData) {
+        float resolution = 32f;
+        float x = (float)layerObject.x/resolution;
+        float y = (float)levelData.height - (float)layerObject.y/resolution;
+
+        return new Vector3 (x, 0f, y);
+      }
+    }
+  }
+}
---
+++ Arch/LevelGeneration/LevelDataLoader.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.IO;    
+
+namespace Arch {
+  namespace LevelGeneration {
+    [System.Serializable]
+    public class LevelData {
+      public int height;
+      public int width;
+      // public int tileHeight;
+      // public int tileWidth;
+      public LayerData[] layers;
+    }
+
+    [System.Serializable]
+    public class LayerData {
+      public string name;
+      public LayerObjectData[] objects;
+    }
+
+    [System.Serializable]
+    public class LayerObjectData {
+      public int gid;
+      public int height;
+      public int width;
+      public int x;
+      public int y;
+      public LayerObjectProperties properties;
+    }
+
+    [System.Serializable]
+    public class LayerObjectProperties {
+      public float show_chance;
+    }
+
+    public class LevelDataLoader : MonoBehaviour {
+      public string levelDataFileName = "levels.json";
+
+    	void Start() {
+    		LoadLevelData ();
+    	}
+
+    	private void LoadLevelData()
+    	{
+        
+     
+    		// Path.Combine combines strings into a file path
+    		// Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
+    		string filePath = Path.Combine(Application.streamingAssetsPath, levelDataFileName);
+
+    		if(File.Exists(filePath))
+    		{
+    			// Read the json from the file into a string
+    			string dataAsJson = File.ReadAllText(filePath); 
+       
+    			// Pass the json to JsonUtility, and tell it to create a levelData object from it
+    			LevelData levelData = JsonUtility.FromJson<LevelData>(dataAsJson);
+
+          for (int layerNum = 0; layerNum < levelData.layers.Length; layerNum++) {
+            LayerData layerData = levelData.layers [layerNum];
+            LayerObjectData[] layerObjects = layerData.objects;
+               
+            for (int objectNum = 0; objectNum < layerObjects.Length; objectNum++) {
+              LayerObjectData layerObject = layerObjects [objectNum];
+
+              SpawnObject (layerObject, levelData);
+            }
+          }
+    		}
+    		else
+    		{
+    			Debug.LogError("Cannot load game data!");
+    		}
+    	}
+
+      private void SpawnObject(LayerObjectData layerObject, LevelData levelData) {
+        LayerObjectSpawner.Spawn (layerObject, levelData);
+      }
+    }
+  }
+}
